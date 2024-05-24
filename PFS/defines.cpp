84a2e91bae4d0e75f4f992242ae0fb327fc766e8
Toudonou <hnounagnonoswald@gmail.com>
2024-05-24 15:33:36 +0200

    Adding vector constants

--- PFS/defines.cpp
+++ PFS/defines.cpp
@@ -4,8 +4,15 @@
 
 #include "defines.h"
 
+Vector2 Vector2::ZERO = {0, 0};
+Vector2 Vector2::UP = {0, -1};
+Vector2 Vector2::DOWN = {0, 1};
+Vector2 Vector2::LEFT = {-1, 0};
+Vector2 Vector2::RIGHT = {1, 0};
+
 std::string randomStringGenerator(const int stringLength) {
-    const std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!\"#%&/()=?@${[]}";
+    const std::string characters =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!\"#%&/()=?@${[]}";
     const int charactersLength = characters.length();
     std::string result;
 
--- PFS/defines.h
+++ PFS/defines.h
@@ -6,11 +6,32 @@
 #define VECTOR2_H
 
 #include <string>
-#include <cstdlib>
 
 struct Vector2 {
     int x;
     int y;
+    static Vector2 ZERO;
+    static Vector2 UP;
+    static Vector2 DOWN;
+    static Vector2 LEFT;
+    static Vector2 RIGHT;
+
+
+    bool operator==(const Vector2 &other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+enum TILE {
+    WALL, EMPTY, TARGET, UP, DOWN, DOWN_LEFT, DOWN_RIGHT, LEFT, RIGHT, UP_LEFT, UP_RIGHT
+};
+
+enum COLOR {
+    UNVISITED, VISITED, PROCESSED
+};
+
+enum MODE {
+    EDIT, SIMULATION
 };
 
 // To generate a random string, especially for the IDs of our components
